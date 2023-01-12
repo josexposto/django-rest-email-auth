985045ff2084ba2203e5c3ef64a924ab4345208a
José Luís Padrão Exposto <exp@ipb.pt>
2023-01-12 12:54:55 +0000

    Change url to path

--- rest_email_auth/urls.py
+++ rest_email_auth/urls.py
@@ -2,7 +2,7 @@
 URL configuration for the ``rest_email_auth`` app.
 """
 
-from django.conf.urls import url
+from django.urls import path, re_path
 
 from rest_email_auth import views
 
@@ -11,29 +11,29 @@ app_name = "rest-email-auth"
 
 
 urlpatterns = [
-    url(r"^emails/$", views.EmailListView.as_view(), name="email-list"),
-    url(
+    re_path(r"^emails/$", views.EmailListView.as_view(), name="email-list"),
+    re_path(
         r"^emails/(?P<pk>[0-9]+)/$",
         views.EmailDetailView.as_view(),
         name="email-detail",
     ),
-    url(r"^register/$", views.RegistrationView.as_view(), name="register"),
-    url(
+    re_path(r"^register/$", views.RegistrationView.as_view(), name="register"),
+    re_path(
         r"^request-password-reset/$",
         views.PasswordResetRequestView.as_view(),
         name="password-reset-request",
     ),
-    url(
+    re_path(
         r"^resend-verification/$",
         views.ResendVerificationView.as_view(),
         name="resend-verification",
     ),
-    url(
+    re_path(
         r"^reset-password/$",
         views.PasswordResetView.as_view(),
         name="password-reset",
     ),
-    url(
+    re_path(
         r"^verify-email/$",
         views.EmailVerificationView.as_view(),
         name="verify-email",
